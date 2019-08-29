22b34153dffeb46bd8e7ac94db72fd359fe4dd0b
p4ll <cornelius.weling@gmail.com>
2019-08-29 12:00:59 +0400

    initial

---
+++ 2/script.py
@@ -0,0 +1,207 @@
+import vrep
+import sys
+import math
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import lib.breezyslam as brs
+
+
+class Pioneer:
+    clientID = -1
+    #10.1, 0.5, 1.5
+    propConst = 10.1
+    integralConst = 1.5
+    diffConst = 0.5
+
+    integralMaxVal = 0.2
+    integralMinVal = -0.2
+    integralSum = 0.0
+    prevDist = -1
+
+    leftWeelSpeed = 1
+    rightWeelSpeed = 1
+
+    reqDist = 0.55
+
+    def erCheck(self, e, str):
+        if e == -1:
+            print('Somthing wrong with {0}'.format(str))
+            sys.exit()
+
+    def addLeftSpeed(self, newSpeed):
+        e = vrep.simxSetJointTargetVelocity(self.clientID, self.leftMotor, self.leftWeelSpeed + newSpeed, vrep.simx_opmode_oneshot_wait)
+        self.erCheck(e, 'leftMotor')
+
+    def addRightSpeed(self, newSpeed):
+        e = vrep.simxSetJointTargetVelocity(self.clientID, self.rightMotor, self.rightWeelSpeed + newSpeed, vrep.simx_opmode_oneshot_wait)
+        self.erCheck(e, 'rightMotor')
+
+    def __init__(self):
+        vrep.simxFinish(-1)
+        self.clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+        if self.clientID == -1:
+            print('Connection not successful')
+            sys.exit('Could not connect')
+        else:
+            print("Connected to remote server")
+        
+        # vrep.simxStartSimulation(self.clientID, vrep.simx_opmode_streaming)
+
+        errorCode, self.sensorFr = vrep.simxGetObjectHandle(self.clientID, 'Prox1', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'Prox1')
+        errorCode, self.sensor = vrep.simxGetObjectHandle(self.clientID, 'Prox2', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'Prox2')
+        
+        errorCode, self.leftMotor = vrep.simxGetObjectHandle(self.clientID, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'leftMotor')
+        errorCode, self.rightMotor = vrep.simxGetObjectHandle(self.clientID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'rightMotor')
+        errorCode, self.sensor = vrep.simxGetObjectHandle(self.clientID, 'fastHokuyo', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'sensor')
+
+        self.addLeftSpeed(0)
+        self.addRightSpeed(0)
+    
+    def getH(self, a, b):
+        c = math.sqrt(a ** 2 + b ** 2)
+        if abs(c - 0) < 0.000001:
+            c = 1.0
+        h = a * b / c
+        return h
+    def getH2(self, a, b):
+        return min(a, b)
+
+    def calulate(self, state, dist):
+        deltaDist = self.reqDist - dist
+
+        propComponent = self.propConst * deltaDist
+
+        self.integralSum = self.integralSum + deltaDist
+        self.integralSum = min(self.integralSum, self.integralMaxVal)
+        self.integralSum = max(self.integralSum, self.integralMinVal)
+        integralComponent = self.integralConst * self.integralSum
+
+        if self.prevDist == -1:
+            self.prevDist = dist
+        
+        diffComponent = self.diffConst * (dist - self.prevDist)
+
+        self.prevDist = dist
+        result = propComponent + diffComponent + integralComponent
+        
+        self.addLeftSpeed(-result)
+        self.addRightSpeed(result)
+        print('dist = {0} speedLeft = {1} speedRight = {2} res = {3}'.format(dist, self.leftWeelSpeed - result, self.rightWeelSpeed + result, result))
+
+    def rotL(self):
+        self.addLeftSpeed(-2.0 * self.leftWeelSpeed)
+        #self.addRightSpeed()
+
+    def simulate(self):
+        while vrep.simxGetConnectionId(self.clientID) != -1:
+            (errorCode, sensorState, sensorDetection, detectedObjectHandle,
+                detectedSurfaceNormalVectorUp) = vrep.simxReadProximitySensor(self.clientID, self.sensor, vrep.simx_opmode_streaming)
+            (errorCode, frontState, frontDetection, detectedObjectHandle,
+                detectedSurfaceNormalVectorFr) = vrep.simxReadProximitySensor(self.clientID, self.sensorFr, vrep.simx_opmode_streaming)
+            if (frontState and sensorState):
+                self.calulate(sensorState, min(sensorDetection[2], frontDetection[2]))
+            elif (frontState):
+                self.calulate(frontState, frontDetection[2])
+            elif (sensorState):
+                self.calulate(sensorState, sensorDetection[2])
+            else:
+                self.calulate(sensorState, self.reqDist + 0.1)
+            time.sleep(0.1)
+
+
+class PioneerSen:
+    clientID = -1
+    #10.1, 0.5, 1.5
+    propConst = 10.1
+    integralConst = 1.5
+    diffConst = 0.5
+
+    integralMaxVal = 0.2
+    integralMinVal = -0.2
+    integralSum = 0.0
+    prevDist = -1
+
+    leftWeelSpeed = 1
+    rightWeelSpeed = 1
+
+    reqDist = 0.55
+
+    def erCheck(self, e, str):
+        if e == -1:
+            print('Somthing wrong with {0}'.format(str))
+            sys.exit()
+
+    def addLeftSpeed(self, newSpeed):
+        e = vrep.simxSetJointTargetVelocity(self.clientID, self.leftMotor, self.leftWeelSpeed + newSpeed, vrep.simx_opmode_oneshot_wait)
+        self.erCheck(e, 'leftMotor')
+
+    def addRightSpeed(self, newSpeed):
+        e = vrep.simxSetJointTargetVelocity(self.clientID, self.rightMotor, self.rightWeelSpeed + newSpeed, vrep.simx_opmode_oneshot_wait)
+        self.erCheck(e, 'rightMotor')
+
+    def __init__(self):
+        vrep.simxFinish(-1)
+        self.clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+        if self.clientID == -1:
+            print('Connection not successful')
+            sys.exit('Could not connect')
+        else:
+            print("Connected to remote server")
+        
+        # vrep.simxStartSimulation(self.clientID, vrep.simx_opmode_streaming)
+
+        # errorCode, self.sensorFr = vrep.simxGetObjectHandle(self.clientID, 'Prox1', vrep.simx_opmode_oneshot_wait)
+        # self.erCheck(errorCode, 'Prox1')
+        # errorCode, self.sensor = vrep.simxGetObjectHandle(self.clientID, 'Prox2', vrep.simx_opmode_oneshot_wait)
+        # self.erCheck(errorCode, 'Prox2')
+        
+        errorCode, self.leftMotor = vrep.simxGetObjectHandle(self.clientID, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'leftMotor')
+        errorCode, self.rightMotor = vrep.simxGetObjectHandle(self.clientID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'rightMotor')
+        errorCode, self.sensor = vrep.simxGetObjectHandle(self.clientID, 'fastHokuyo', vrep.simx_opmode_oneshot_wait)
+        self.erCheck(errorCode, 'sensor')
+
+        self.addLeftSpeed(0)
+        self.addRightSpeed(0)
+    
+
+    def calulate(self, state, dist):
+        deltaDist = self.reqDist - dist
+
+        propComponent = self.propConst * deltaDist
+
+        self.integralSum = self.integralSum + deltaDist
+        self.integralSum = min(self.integralSum, self.integralMaxVal)
+        self.integralSum = max(self.integralSum, self.integralMinVal)
+        integralComponent = self.integralConst * self.integralSum
+
+        if self.prevDist == -1:
+            self.prevDist = dist
+        
+        diffComponent = self.diffConst * (dist - self.prevDist)
+
+        self.prevDist = dist
+        result = propComponent + diffComponent + integralComponent
+        
+        self.addLeftSpeed(-result)
+        self.addRightSpeed(result)
+        #print('dist = {0} speedLeft = {1} speedRight = {2} res = {3}'.format(dist, self.leftWeelSpeed - result, self.rightWeelSpeed + result, result))
+
+    def simulate(self):
+        a = 10
+        while (True): # 'measuredDataAtThisTime'
+            data = vrep.simxGetStringSignal(self.clientID, 'measuredDataAtThisTime', vrep.simx_opmode_oneshot_wait) #simx_opmode_streaming
+            meaData = vrep.simxUnpackFloats(data)
+            print(meaData)
+
+
+if __name__ == "__main__":
+    pio = PioneerSen()
+    pio.simulate()
