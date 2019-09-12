c4b1b4ba53986d4e9cb3879e89cb66bf696c1861
p4ll <cornelius.weling@gmail.com>
2019-09-13 00:44:21 +0400

    add slamTest

--- 2/src/settings.py
+++ 2/src/settings.py
@@ -6,7 +6,7 @@ class PioLaser(Laser):
     
     def __init__(self):
         # detection_margin=70, offset_mm=145
-        Laser.__init__(self, 684, 10, 240, 5000, detection_margin=70, offset_mm=145)
+        Laser.__init__(self, 684, 10, 240, 20000, detection_margin=70, offset_mm=145) #detection_margin=70
         # URG04LX.__init__(self, 70, 145)
         
 
@@ -26,9 +26,7 @@ class PioRobot(WheeledVehicle):
         
         return WheeledVehicle.computePoseChange(self, timestamp, leftWheelOdometry, rightWheelOdometry)
 
-    def extractOdometry(self, timestamp, leftWheel, rightWheel):
-                
-        # Convert microseconds to seconds, ticks to angles        
+    def extractOdometry(self, timestamp, leftWheel, rightWheel):     
         return timestamp, \
                self._rad_to_degrees(leftWheel), \
                self._rad_to_degrees(rightWheel)
@@ -39,7 +37,7 @@ class PioRobot(WheeledVehicle):
                (odometry[0], odometry[1], odometry[2])
                
     def _rad_to_degrees(self, rad):
-        return rad * 57.2958
+        return rad * 100#57.2958
 
     def _ticks_to_degrees(self, ticks):
         
--- 2/src/slamTest.py
+++ 2/src/slamTest.py
@@ -14,33 +14,9 @@ from settings import PioLaser, PioRobot
 from threading import Thread
 
 
-def func(clientID, robot, slam, pose, mapbytes):
-    while (1):
-        data = vrep.simxGetStringSignal(clientID, 'measuredDataAtThisTime', vrep.simx_opmode_streaming)
-        dataDists = vrep.simxGetStringSignal(clientID, 'dataDistsAtThisTime', vrep.simx_opmode_streaming)
-        rawOdometry = vrep.simxGetStringSignal(clientID, 'odometryAtThisTime', vrep.simx_opmode_streaming)
-
-        odometry = vrep.simxUnpackFloats(rawOdometry[1])
-        meaData = vrep.simxUnpackFloats(data[1])
-        dists = vrep.simxUnpackFloats(dataDists[1])
-
-        if len(dists) == 0 or len(odometry) == 0:
-            continue
-
-        for i in range(len(dists)):
-            dists[i] = (dists[i] * 1000)
-
-        velocities = robot.computePoseChange(time.time(), odometry[0], odometry[1])
-        slam.update(dists, velocities)
-        pose[0], pose[1], pose[2] = slam.getpos()
-        print(dists)
-        slam.getmap(mapbytes)
-
-        time.sleep(0.01)
-
 class Slam():
     MAP_SIZE_PIXELS = 800#800 # 1000
-    MAP_SIZE_METERS = 50#40
+    MAP_SIZE_METERS = 32#40
 
     propConst = 10.1 # 10.1
     integralConst = 1.5
@@ -118,12 +94,11 @@ class Slam():
         self.erCheck(errorCode, 'sensorFH')
 
         self.mapbytes = bytearray(self.MAP_SIZE_PIXELS * self.MAP_SIZE_PIXELS)
-        # self.laser = URG04LX(70, 145)
         self.laser = PioLaser()
         self.robot = PioRobot()
-        self.slam = RMHC_SLAM(self.laser, self.MAP_SIZE_PIXELS, self.MAP_SIZE_METERS, random_seed = 9999, map_quality=1, sigma_xy_mm=20, sigma_theta_degrees=20) #map_quality=255 sigma_theta_degrees=5, sigma_xy_mm=10
-        self.viz = MapVisualizer(self.MAP_SIZE_PIXELS, self.MAP_SIZE_METERS, "1")
-        self.pose = [30000, 30000, 0]
+        self.slam = RMHC_SLAM(self.laser, self.MAP_SIZE_PIXELS, self.MAP_SIZE_METERS, random_seed = 9999, map_quality=1)
+        self.viz = MapVisualizer(self.MAP_SIZE_PIXELS, self.MAP_SIZE_METERS, "1", True)
+        self.pose = [0, 0, 0]
     
     def erCheck(self, e, str):
         if e == -1:
@@ -132,23 +107,8 @@ class Slam():
 
     def simulate(self):
         prevTime = 0
-        # thread = Thread(target=func, args=(self.clientID, self.robot, self.slam, self.pose, self.mapbytes))
-        # thread.daemon = True
-        # thread.start()
 
         while vrep.simxGetConnectionId(self.clientID) != -1:
-            # if keyboard.is_pressed('j'):
-            #     self.addLeftSpeed(1)
-            #     self.addRightSpeed(1)
-            # elif keyboard.is_pressed('k'):
-            #     self.addLeftSpeed(0)
-            #     self.addRightSpeed(0)
-            # elif keyboard.is_pressed('h'):
-            #     self.addLeftSpeed(0)
-            #     self.addRightSpeed(1)
-            # elif keyboard.is_pressed('l'):
-            #     self.addLeftSpeed(1)
-            #     self.addRightSpeed(0)
             
             (errorCode, sensorState, sensorDetection, detectedObjectHandle,
                 detectedSurfaceNormalVectorUp) = vrep.simxReadProximitySensor(self.clientID, self.sensor, vrep.simx_opmode_streaming)
@@ -163,7 +123,7 @@ class Slam():
             else:
                 self.calulate(sensorState, self.reqDist + 0.1)
 
-            data = vrep.simxGetStringSignal(self.clientID, 'measuredDataAtThisTime', vrep.simx_opmode_streaming) #simx_opmode_streaming
+            data = vrep.simxGetStringSignal(self.clientID, 'measuredDataAtThisTime', vrep.simx_opmode_streaming)
             dataDists = vrep.simxGetStringSignal(self.clientID, 'dataDistsAtThisTime', vrep.simx_opmode_streaming)
             rawOdometry = vrep.simxGetStringSignal(self.clientID, 'odometryAtThisTime', vrep.simx_opmode_streaming)
 
@@ -175,24 +135,15 @@ class Slam():
 
             for i in range(len(dists)):
                 dists[i] = (dists[i] * 1000)
-                # if dists[i] == 4000:
-                #     dists[i] = 0
 
             velocities = self.robot.computePoseChange(time.time(), odometry[0], odometry[1])
-            self.slam.update(dists, velocities) #velocities
+            self.slam.update(dists) #velocities
             self.pose[0], self.pose[1], self.pose[2] = self.slam.getpos()
             self.slam.getmap(self.mapbytes)
 
-            # curTime = time.time()
-            # if prevTime > 0:
-            #     time.sleep(curTime - prevTime)
-            # prevTime = curTime
-            # self.pose[2] -= velocities[1]
-            # self.pose[2] -= 30
-            print(f"Odo: {odometry[0], odometry[1]}, vel: {velocities}б pos: {self.slam.getpos()}")
+            print(f"Odo: {odometry[0], odometry[1]}, vel: {velocities}, pos: {self.slam.getpos()}")
             if not self.viz.display(self.pose[0]/1000., self.pose[1]/1000., self.pose[2], self.mapbytes):
                 exit(0)
-            # time.sleep(0.1)
 
 if __name__ == "__main__":
     slam = Slam()
